392e1d1c24676068308a255ed92675c584b3c08d
Rasmus Aron <rasmaro@users.noreply.github.com>
2018-09-11 14:22:45 +0300

    laadime midagi siis esimest korda üles

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	<meta charset="utf-8">
+	<title>Rasmus Aron, õppetöö</title>
+   </head>
+   <body>
+   <h1> Rasmus Aron </h1>
+   <p>Minu <a href = "https://www.tlu.ee/en" >TLÜ</a>  </p>
+   
+   <p> Listen. Strange women lying in ponds distributing swords is no basis for a system of government.<br /> Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony.<br />You can't expect to wield supreme executive power just 'cause some watery tart threw a sword at you! <br />I mean, if I went around saying I was an emperor just because some moistened bint had lobbed a scimitar at me, they'd put me away!</p>
+   
+   <img src="../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_2.jpg" alt="TLÜ Terra õppehoone" align = "right" img{border-radius: 50%;}>
+   <p>Minu sõber teeb ka <a href = "../~raitvai" target ="_blank">veebi<a></p>
+   </body>
+</html>
